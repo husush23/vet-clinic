aaff1c1cf8eb92e0b0e3ba41d8f8260f113b8d14
husush23 <husseinkadare40@gmail.com>
2023-07-18 14:22:53 +0300

    updated

--- queries.sql
+++ queries.sql
@@ -5,15 +5,16 @@ SELECT * FROM animals WHERE name LIKE '%mon'
 SELECT * FROM animals
 WHERE date_of_birth   BETWEEN '2016-01-01' AND '2019-01-01'
 
-SELECT * FROM animals WHERE escape_attempts < 3
+SELECT * FROM animals
+WHERE escape_attempts < 3 AND neutered =true
 
-SELECT * FROM animals WHERE name = 'Pikachu' OR name = 'Agumon'
+SELECT name, date_of_birth FROM animals
+WHERE name = 'Pikachu' OR name = 'Agumon'
 
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5
 
-SELECT * FROM animals
+SELECT * FROM animals WHERE neutered =true
 
-WHERE neutered = false
 SELECT * FROM animals WHERE name != 'Gabumon'
 
 SELECT * FROM animals
