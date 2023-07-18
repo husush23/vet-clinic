d832575268e060bd7b16ca6c5b8af578c2207f63
husush23 <husseinkadare40@gmail.com>
2023-07-18 13:34:06 +0300

    Table created

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+ INSERT INTO animals(name, date_of_birth, escape_attempts, weight_kg, neutered)
+VALUES ('Agumon', '2020-02-03', 0, 10.23, true),
+('Gabumon', '2018-11-15', 2, 8, true),
+('Pikachu', '2021-01-07', 1, 15.04, flase),
+('Devimon','2017-05-12', 5, 11, false )
--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals(
+ id SERIAL PRIMARY KEY,
+ name VARCHAR(100) NOT NULL,
+ date_of_birth DATE NOT NULL,
+ escape_attempts INTEGER NOT NULL,
+ weight_kg DECIMAL(10,2)
+ neutered BOOLEAN
+)
