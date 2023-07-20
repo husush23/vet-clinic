950e99aeff46120a2c08f83b9f820ba177922858
husush23 <husseinkadare40@gmail.com>
2023-07-20 16:38:28 +0300

    Updated files

--- data.sql
+++ data.sql
@@ -3,18 +3,17 @@
  INSERT INTO animals(name, date_of_birth, escape_attempts, weight_kg, neutered)
 VALUES ('Agumon', '2020-02-03', 0, 10.23, true),
 ('Gabumon', '2018-11-15', 2, 8, true),
-('Pikachu', '2021-01-07', 1, 15.04, flase),
-('Devimon','2017-05-12', 5, 11, false )
+('Pikachu', '2021-01-07', 1, 15.04, false),
+('Devimon','2017-05-12', 5, 11, false );
 
 INSERT INTO animals(name, date_of_birth, escape_attempts, weight_kg, neutered)
-
 VALUES ('Charmander', '2020-02-08', 0, -11, false),
 ('Plantmon', '2021-11-15',2,  -5.7, true ),
 ('Squirtle', '1993-04-02',  3, -12.13, false),
 ('Angemon', '2005-06-12', 1,-45, true),
 ('Boarmon', '2005-06-07', 7, 20.4, true),
 ('Blossom', '1998-10-13', 3, 17, true),
-('Ditto', '2022-05-14', 4, 22, true)
+('Ditto', '2022-05-14', 4, 22, true);
 
 /* Day 3*/
 INSERT INTO owners(full_name, age)
@@ -24,46 +23,37 @@ VALUES
 ('Bob', 45),
 ('Melody Pond', 77),
 ('Dean WinchesteR', 14),
-('Jodie Whittaker', 38)
+('Jodie Whittaker', 38);
 
 INSERT INTO species(name)
 VALUES
 ('Pokemon'),
-('Digimon')
-
-
-UPDATE animals SET species_id = (
- CASE
-    WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
-    ELSE
-        (SELECT id FROM species WHERE name = 'Pokemon')
- END
-);
+('Digimon');
 
 UPDATE animals SET  owner_id = (
 SELECT id FROM owners WHERE full_name = 'Sam Smith'
 )
-WHERE name IN ('Agumon' )
+WHERE name IN ('Agumon' );
 
 UPDATE animals SET  owner_id = (
 SELECT id FROM owners WHERE full_name = 'Jennifer Orwell'
 )
-WHERE name IN ('Pikachu', 'Gabumon' )
+WHERE name IN ('Pikachu', 'Gabumon' );
 
 UPDATE animals SET  owner_id = (
 SELECT id FROM owners WHERE full_name = 'Bob'
 )
-WHERE name IN ( 'Devimon', 'Plantmon' )
+WHERE name IN ( 'Devimon', 'Plantmon' );
 
 UPDATE animals SET owner_id = (
     SELECT id FROM owners WHERE full_name ='Melody Pond'
 )
-WHERE name IN('Charmander', 'Squirtle',  'Blossom')
+WHERE name IN('Charmander', 'Squirtle',  'Blossom');
 
 UPDATE animals SET owner_id = (
     SELECT id FROM owners WHERE full_name ='Dean Winchester'
 )
-WHERE name IN('Angemon', 'Boarmon')
+WHERE name IN('Angemon', 'Boarmon');
 
 -- Day 4
 INSERT INTO vets(name, age, date_of_graduation)
@@ -71,7 +61,7 @@ VALUES
 ('William Tatcher', 45, '2000-04-23'),
 ('Maisy Smith', 26, '2019-01-17'),
 ('Stephanie Mendez', 64, '1981-05-14'),
-('Jack Harkness', 38, '2008-06-08'),
+('Jack Harkness', 38, '2008-06-08');
 
 INSERT INTO specializations(vet_id, species_id)
 VALUES
@@ -101,7 +91,4 @@ VALUES
 ((SELECT id FROM animals WHERE name='Boarmon'), (SELECT id FROM vets WHERE name='Maisy Smith'),'2020-02-27'),
 ((SELECT id FROM animals WHERE name='Boarmon'), (SELECT id FROM vets WHERE name='Maisy Smith'),'2020-08-03'),
 ((SELECT id FROM animals WHERE name='Blossom'), (SELECT id FROM vets WHERE name='Stephanie Mendez'),'2020-05-24'),
-((SELECT id FROM animals WHERE name='Blossom'), (SELECT id FROM vets WHERE name='William Tatcher'),'2021-01-11')
-
-
-
+((SELECT id FROM animals WHERE name='Blossom'), (SELECT id FROM vets WHERE name='William Tatcher'),'2021-01-11');
--- schema.sql
+++ schema.sql
@@ -5,35 +5,35 @@ CREATE TABLE animals(
  name VARCHAR(100) NOT NULL,
  date_of_birth DATE NOT NULL,
  escape_attempts INTEGER NOT NULL,
- weight_kg DECIMAL(10,2)
+ weight_kg DECIMAL(10,2),
  neutered BOOLEAN
-)
+);
 
 /* DAY 2*/
 -- Add new column to the table
 ALTER TABLE animals
-ADD COLUMN species VARCHAR(50)
+ADD COLUMN species VARCHAR(50);
 
 /*DAY 3*/
 CREATE TABLE owners(
     id SERIAL PRIMARY KEY,
     full_name VARCHAR(100),
     age INTEGER
-)
+);
 
 CREATE TABLE species(
     id SERIAL PRIMARY KEY,
     name VARCHAR(100)
-)
+);
 
 -- Remove Species column
-ALTER TABLE animals DROP COLUMN species
+ALTER TABLE animals DROP COLUMN species;
 
 -- Adding species_id
 ALTER TABLE animals ADD species_id INT  REFERENCES species(id);
 
 -- Adding owner_id
-ALTER TABLE animals ADD owner_id INT REFERENCES owners(id)
+ALTER TABLE animals ADD owner_id INT REFERENCES owners(id);
 
 -- Day 4
 
@@ -42,15 +42,15 @@ CREATE TABLE vets(
     name VARCHAR(50) NOT NULL,
     age INTEGER NOT NULL,
     date_of_graduation DATE 
-)
+);
 CREATE TABLE specializations (
-    vet_id INT REFERENCES vets(vet_id)
-    species_id INT REFERENCES species(id)
+    vet_id INT REFERENCES vets(id),
+    species_id INT REFERENCES species(id),
     PRIMARY KEY(vet_id, species_id)
-)
+);
 
 CREATE TABLE visits (
     animal_id INT REFERENCES animals(id),
     vet_id INT  REFERENCES vets(id),
     date_of_visit DATE 
-)
+);
